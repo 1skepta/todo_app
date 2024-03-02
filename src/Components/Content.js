5293bb5639fac7d561f2dff04977f2ba1c8d0f23
darrk-heart <prospaul999@gmail.com>
2024-03-02 04:05:42 +0000

    working

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,46 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ThemeHead.css";
 import cross from "../assets/icon-cross.svg";
 
 function Content() {
+  const [todos, setTodos] = useState([
+    "Complete online javascript course",
+    "Jog around the park 3x",
+    "10 minutes meditation",
+    "Read for 1 hour",
+    "Pick up groceries",
+  ]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div>
+    <div className="container">
       <div className="addTodo">
         <div>
           <span className="O"></span>
-          <input type="text" placeholder="Create a new todo..." />
+          <input
+            type="text"
+            placeholder="Create a new todo..."
+            value={newTodo}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
         </div>
       </div>
       <div className="listTodo">
-        <div className="items">
-          <span className="O o"></span>
-          <span className="text">Complete online javascript course</span>
-          <img className="cross" src={cross} alt="cross" />
-        </div>
-        <div className="items">
-          <span className="O o"></span>
-          <span className="text">Jog around the park 3x</span>
-          <img className="cross" src={cross} alt="cross" />
-        </div>
-        <div className="items">
-          <span className="O o"></span>
-          <span className="text">10 minutes meditation</span>
-          <img className="cross" src={cross} alt="cross" />
-        </div>
-        <div className="items">
-          <span className="O o"></span>
-          <span className="text">Read for 1 hour</span>
-          <img className="cross" src={cross} alt="cross" />
-        </div>
-        <div className="items">
-          <span className="O o"></span>
-          <span className="text">Pick up groceries</span>
-          <img className="cross" src={cross} alt="cross" />
-        </div>
-
+        {todos.map((todo, index) => (
+          <div className="items" key={index}>
+            <span className="O o"></span>
+            <span className="text">{todo}</span>
+            <img
+              className="cross"
+              src={cross}
+              alt="cross"
+              onClick={() => removeTodo(index)}
+            />
+          </div>
+        ))}
         <div className="actions">
           <div className="one">
-            <span>5 items left</span>
+            <span>{todos.length} items left</span>
           </div>
           <div className="two">
             <span>All</span>
