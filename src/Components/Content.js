86100f3b6ff47a4807488f30f582bd1e93475ca4
darrk-heart <prospaul999@gmail.com>
2024-03-02 02:41:32 +0000

    more changes

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -12,8 +12,24 @@ function Content() {
       </div>
       <div className="listTodo">
         <div>
-          <span className="O"></span>
-          <span>Complete online javascript course</span>
+          <span className="O o"></span>
+          <span className="text">Complete online javascript course</span>
+        </div>
+        <div>
+          <span className="O o"></span>
+          <span className="text">Jog around the park 3x</span>
+        </div>
+        <div>
+          <span className="O o"></span>
+          <span className="text">10 minutes meditation</span>
+        </div>
+        <div>
+          <span className="O o"></span>
+          <span className="text">Read for 1 hour</span>
+        </div>
+        <div>
+          <span className="O o"></span>
+          <span className="text">Pick up groceries</span>
         </div>
       </div>
     </div>
