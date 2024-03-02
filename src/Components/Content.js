3272002f88aee675a1ebf1a33d30a058c4c97f47
darrk-heart <prospaul999@gmail.com>
2024-03-02 03:01:44 +0000

    working

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -11,25 +11,39 @@ function Content() {
         </div>
       </div>
       <div className="listTodo">
-        <div>
+        <div className="items">
           <span className="O o"></span>
           <span className="text">Complete online javascript course</span>
         </div>
-        <div>
+        <div className="items">
           <span className="O o"></span>
           <span className="text">Jog around the park 3x</span>
         </div>
-        <div>
+        <div className="items">
           <span className="O o"></span>
           <span className="text">10 minutes meditation</span>
         </div>
-        <div>
+        <div className="items">
           <span className="O o"></span>
           <span className="text">Read for 1 hour</span>
         </div>
-        <div>
+        <div className="items">
           <span className="O o"></span>
           <span className="text">Pick up groceries</span>
+        </div>
+
+        <div className="actions">
+          <div className="one">
+            <span>5 items left</span>
+          </div>
+          <div className="two">
+            <span>All</span>
+            <span>Active</span>
+            <span>Completed</span>
+          </div>
+          <div className="three">
+            <span>Clear Completed</span>
+          </div>
         </div>
       </div>
     </div>
